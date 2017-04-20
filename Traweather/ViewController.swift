6b5ab09a5e5216320947d8e3bb0de8da40d94876
Jiayi Zou <harry.zou.0511@gmail.com>
2017-04-20 14:57:02 -0400

    Initial Commit

---
+++ Traweather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Traweather
+//
+//  Created by Jiayi Zou on 4/20/17.
+//  Copyright © 2017 SU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
